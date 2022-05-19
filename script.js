2551ff479f4847fbe78825d6348dab3d1c83c58a
Rayaa <rghirri@gmail.com>
2022-05-19 10:26:25 +0100

    Fixed button bug

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var button = document.getElementById('theButton');
 
 //add event listener
-button.addEventListener('click', function(event) {
-  alort("You are awesome!");
+button.addEventListener('click', function (event) {
+  alert('You are awesome!');
 });
